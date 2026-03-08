3b983939045c6f0f3145f44f6664ee72e18ef750
Mohamed Faizan <mohamedfaizan414@gmail.com>
2026-03-08 19:40:35 +0530

    ping created

--- express backend/server.js
+++ express backend/server.js
@@ -3,7 +3,9 @@ const connectDB = require("./src/db/db.js")
 
 connectDB()
 
-
+app.get('/pings',(req,res)=>{
+    res.json({message: "server is active"})
+})
 
 app.listen(3000,()=>{
     console.log("Server started")
