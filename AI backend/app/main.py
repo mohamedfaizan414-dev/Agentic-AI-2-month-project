1b1b5975657325d89c69d9d9a8e65670c10ff927
Mohamed Faizan <mohamedfaizan414@gmail.com>
2026-03-08 15:06:08 +0530

    Updated API routes and integrations

--- AI backend/app/main.py
+++ AI backend/app/main.py
@@ -2,10 +2,17 @@
 
 from fastapi import FastAPI
 from api import router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Travel Agent AI",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(router)
